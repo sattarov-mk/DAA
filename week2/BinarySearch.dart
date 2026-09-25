54628a4432481c4dfaebb061ac0c8c3ef7eab997
Sattarov Mukhamed <muhammedsattarov4@gmail.com>
2026-09-25 13:05:19 +0600

    Created search

---
+++ week2/BinarySearch.dart
@@ -0,0 +1,14 @@
+int search(List<int> nums, int target) {
+  int left = 0;
+  inr right = nums.length - 1;
+  while (left <= right) {
+    int mid = left + (right - left) ~/2;
+    if (nums[mid] == target) {
+      return mid;
+    } else if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  } return -1;
+}
