528f86e7d9ed6e8887f61da57b5ca61b75b0bfb0
Sattarov Mukhamed <muhammedsattarov4@gmail.com>
2026-09-25 14:52:50 +0600

    Произошла опечатка вместо int написал inr

--- week2/BinarySearch/Solution.dart
+++ week2/BinarySearch/Solution.dart
@@ -1,7 +1,7 @@
 class Solution{
 int search(List<int> nums, int target) {
   int left = 0;
-  inr right = nums.length - 1;
+  int right = nums.length - 1;
   while (left <= right) {
     int mid = left + (right - left) ~/2;
     if (nums[mid] == target) {
